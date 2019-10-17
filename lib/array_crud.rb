f2765f8106f8656956c98a0461de3161c96a3f32
Guligena Aierken <guligena_aierken@163.com>
2019-10-17 03:32:25 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -36,8 +36,8 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-toddler_toys=["wow","playdoh","train","arrays!"]
-toddler_toys[0]
+toddler_toys=["wow","playdoh","am","arrays!"]
+toddler_toys[2]
 end
 
 def retrieve_first_element_from_array(array)
