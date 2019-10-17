dc555f525d413b8d93d8560c5de5accf2a14fdd0
Guligena Aierken <guligena_aierken@163.com>
2019-10-17 03:36:06 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -51,5 +51,6 @@ def retrieve_last_element_from_array(array)
 end
 
 def update_element_from_index(array, index_number, element)
-
+  oddler_toys=["wow","playdoh","am","arrays!"]
+  toddler_toys[2]="totally"
 end
